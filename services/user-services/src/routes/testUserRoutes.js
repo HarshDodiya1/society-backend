62ac2415dc2c1be2a603527e7069162454263c57
jay-patel-shivalik <jay@shivalikgroup.com>
2025-11-06 13:01:22 +0530

    changes

--- services/user-services/src/routes/testUserRoutes.js
+++ services/user-services/src/routes/testUserRoutes.js
@@ -4,6 +4,7 @@ const UserValidation = require("../validations/userValidation");
 const router = require('express').Router();
 const { isCommonUserAuthenticated, adminVerifyToken, superAdminVerifyToken } = require("../middleware/authJwt");
 
+// Add just final test deploy
 router.post("/test-user-api", UserValidation.testUserApi, TestUserController.testUserApi);
 
 // router.put("/test-user-api/:userId", UserValidation.update, TestUserController.update);
